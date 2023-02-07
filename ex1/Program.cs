d1038fac1b4c603d1fe5afe2552fcb2b6af48c81
Kondrat <yo126@yandex.ru>
2023-02-07 23:54:44 +0300

    zadacha_1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -3,7 +3,7 @@
 // 2, 4 -> 16
 
 
-int PutNumber(string text) 
+int PutNumber(string text)
 {
     System.Console.Write(text);
     return Convert.ToInt32(Console.ReadLine());
@@ -12,8 +12,8 @@ int PutNumber(string text)
 int GetStepen(int a, int b)
 {
     int result = 1;
-    
-    for (int i = 1; i<=b; i++)
+
+    for (int i = 1; i <= b; i++)
     {
         result = result * a;
     }
