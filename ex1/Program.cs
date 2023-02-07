25ade794819842335ffc85ed1a25404d87f5b4f5
Kondrat <yo126@yandex.ru>
2023-02-07 23:53:55 +0300

    Первая задача

---
+++ ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int PutNumber(string text) 
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetStepen(int a, int b)
+{
+    int result = 1;
+    
+    for (int i = 1; i<=b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+int num = PutNumber("Введите число A ");
+int num2 = PutNumber("Введите число B ");
+int Stepen = GetStepen(num, num2);
+
+
+System.Console.WriteLine($"{num} в степени {num2} = {Stepen}");
